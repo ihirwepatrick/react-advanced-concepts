bf4d29d48f95a7082541928207291594a8e77b8e
Patrick Ihirwe <nipcts3@gmail.com>
2024-08-13 09:16:26 +0200

    configured tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './globals.css';
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline  decoration-sky-500">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
