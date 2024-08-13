1919d416229590986342bdab9f658ece5dad551d
Patrick Ihirwe <nipcts3@gmail.com>
2024-08-13 10:54:32 +0200

    created the signup

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+import { Button } from "../../../@/components/ui/button";
 function SignupForm() {
-  return <div>SignupForm</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default SignupForm;
