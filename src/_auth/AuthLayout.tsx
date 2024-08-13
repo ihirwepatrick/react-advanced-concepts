188cde845d68e74662526ab66d09de4c81bad246
Patrick Ihirwe <nipcts3@gmail.com>
2024-08-13 11:16:27 +0200

    added the assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 function AuthLayout() {
-  return <div>AuthLayout</div>;
+  const isAunthenticated = false;
+  return (
+    <>
+      {isAunthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col">
+            <Outlet />
+          </section>
+          <img />
+        </>
+      )}
+    </>
+  );
 }
 
 export default AuthLayout;
